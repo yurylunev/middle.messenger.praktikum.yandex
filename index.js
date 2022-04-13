bb6c3292135ba3871bd9eb1e97532a5fd87737a5
Yury Lunev <xalt@mail.ru>
2022-04-13 10:32:40 +0300

    Add history-api-fallback

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const fallback = require('express-history-api-fallback');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/', express.static(`${__dirname}/dist`));
 app.use('/static/', express.static(`${__dirname}/static`));
+app.use(fallback('/', {root: `${__dirname}/dist`}));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
