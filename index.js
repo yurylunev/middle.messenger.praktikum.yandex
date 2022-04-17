c8a19d9e44ca217c23d53bf3310feae1ba27c540
Yury Lunev <xalt@mail.ru>
2022-04-17 18:30:03 +0300

    Fix error: NOT 3000 PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fallback = require('express-history-api-fallback');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use('/', express.static(`${__dirname}/dist`));
 app.use('/static/', express.static(`${__dirname}/static`));
